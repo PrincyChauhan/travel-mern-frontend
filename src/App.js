f61842166ed7ff32cb743422123ea2926c56cb4d
PrincyChauhan <chauhanprincee7@gmail.com>
2023-03-21 23:21:24 +0530

    Navigation added

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import {
   Switch,
 } from "react-router-dom";
 import Users from "./user/pages/Users";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <h1>New Place Page</h1>
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <h1>New Place Page</h1>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };

99cc8000ed6a013dabf1aab12acbac46dd61feab
PrincyChauhan <chauhanprincee7@gmail.com>
2023-03-22 21:53:40 +0530

    Side drawer added

---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./SideDrawer.css";
+
+const SideDrawer = (props) => {
+    return <aside className="side-drawer">
+        {props.children}
+    </aside>
+};
+export default SideDrawer;

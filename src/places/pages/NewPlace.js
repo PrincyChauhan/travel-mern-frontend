51d116bd7df75a45e182f1a5e932b58c774cb89f
PrincyChauhan <chauhanprincee7@gmail.com>
2023-04-01 15:42:21 +0530

    Manage state input Component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import Input from '../../shared/components/FormElements/Input';
-import './NewPlace.css';
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter valid Title"
+      />
     </form>
   );
 };

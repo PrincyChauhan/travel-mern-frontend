b431b9c3eb1272dfa5609ed8db9f115a220a1f6f
PrincyChauhan <chauhanprincee7@gmail.com>
2023-04-01 12:39:11 +0530

    Render UserPlace

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
-} from "react-router-dom";
-import Users from "./user/pages/Users";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
+  Switch
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
@@ -17,8 +20,11 @@ const App = () => {
           <Route path="/" exact>
             <Users />
           </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
           <Route path="/places/new" exact>
-            <h1>New Place Page</h1>
+            <NewPlace />
           </Route>
           <Redirect to="/" />
         </Switch>

70b5011a3f10d50ae46a0326d2e1fa3f1faa9349
PrincyChauhan <chauhanprincee7@gmail.com>
2023-04-02 14:17:10 +0530

    Added Address in add place form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -2,7 +2,6 @@ import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
-
 import "./NewPlace.css";
 import {
   VALIDATOR_MINLENGTH,
@@ -57,14 +56,19 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the backend!
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
         type="text"
         label="Title"
-        validators={[VALIDATOR_REQUIRE]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
         onInput={inputHandler}
       />
@@ -74,6 +78,14 @@ const NewPlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>

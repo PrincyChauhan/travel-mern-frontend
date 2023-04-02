4216a8c005917cfe83b25b766417c9ce9f041583
PrincyChauhan <chauhanprincee7@gmail.com>
2023-04-02 15:43:12 +0530

    Hook created but it has error

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,66 +1,34 @@
-import React, { useCallback, useReducer } from "react";
+import React from 'react';
 
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import "./PlaceForm.css";
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 import {
-  VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
-} from "../../shared/components/util/validators";
-
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid },
-        },
-        isValid: formIsValid,
-      };
-    default:
-      return state;
-  }
-};
+  VALIDATOR_MINLENGTH
+} from '../../shared/';
+import {useForm}  from '../../shared/components/hooks/from-hook';
+import './PlaceForm.css';
 
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
+  const [formState, inputHandler] = useForm(
+    {
       title: {
-        value: "",
-        isValid: false,
+        value: '',
+        isValid: false
       },
       description: {
-        value: "",
-        isValid: false,
+        value: '',
+        isValid: false
       },
       address: {
-        value: "",
-        isValid: false,
-      },
+        value: '',
+        isValid: false
+      }
     },
-    isValid: false,
-  });
+    false
+  );
 
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: "INPUT_CHANGE",
-      value: value,
-      isValid: isValid,
-      inputId: id,
-    });
-  }, []);
-
-  const placeSubmitHandler = (event) => {
+  const placeSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs); // send this to the backend!
   };
--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,77 +1,41 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import {
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
-} from "../../shared/components/util/validators";
-import "./PlaceForm.css";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u1",
+    creator: 'u1'
   },
   {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u2",
-  },
+    creator: 'u2'
+  }
 ];
 
-const UpdatePlace = () => {
-  const placeId = useParams().placeId;
-  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
-  if (!identifiedPlace) {
-    return (
-      <div className="center">
-        <h2>Could Not find place</h2>
-      </div>
-    );
-  }
-  return (
-    <form className="place-form">
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        lable="Title"
-        validator={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter valid title"
-        onInput={() => {}}
-        value={identifiedPlace.title}
-        valid={true}
-      ></Input>
-      <Input
-        id="description"
-        element="textarea"
-        lable="Description"
-        validator={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter valid description"
-        onInput={() => {}}
-        value={identifiedPlace.description}
-        valid={true}
-      ></Input>
-      <Button type="submit" disable={true}>UPDATE PLACE</Button>
-    </form>
-  );
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
-export default UpdatePlace;
+export default UserPlaces;
